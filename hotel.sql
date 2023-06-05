a60fa0e3f1552bc2556b72e0eba74ac46ea9f888
lokenmg <104791261+lokenmg@users.noreply.github.com>
2023-06-05 17:06:10 -0600

    Update hotel.sql

--- hotel.sql
+++ hotel.sql
@@ -1,12 +1,11 @@
 CREATE TABLE dispositivos (
-	ip_dispositivo VARCHAR(15) PRIMARY KEY NOT NULL,
+    ip_dispositivo VARCHAR(15) PRIMARY KEY NOT NULL,
     tipo_dis VARCHAR(30),
     estado_dis BOOLEAN
 );
 
 CREATE TABLE habitaciones (
-	n_habitacion INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    id_estado_hab INT,
+    n_habitacion INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     estado_hab BOOLEAN
 );
 
